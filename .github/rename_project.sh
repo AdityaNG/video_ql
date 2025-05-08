36dcd667bdc16376c5f51006313b460e4bf6f101
AdityaNG <AdityaNG@users.noreply.github.com>
2025-05-08 15:26:21 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="AdityaNG"
+original_name="video_ql"
+original_urlname="video_ql"
+original_description="Awesome video_ql created by AdityaNG"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv video_ql $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from video_ql.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "video_ql"
